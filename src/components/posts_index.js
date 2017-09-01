91df39c6c85d178cfcc55d7bdd7650a00f84d155
James Attard <info@jamesattard.com>
2017-09-01 20:02:18 +0200

    Added redux-form

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,27 +1,33 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { fetchPosts } from '../actions';
 
 class PostsIndex extends Component {
-componentDidMount() {
-  this.props.fetchPosts();
-}
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
 
-renderPosts() {
-  // Standard map only iterates over arrays but posts is an object of objects! So we use lodash
-  _.map(this.props.posts, post => {
-    return (
-      <li key={post.id} className="list-group-item">
-        {post.title}
-      </li>
-    );
-  }); 
-}
+  renderPosts() {
+    // Standard map only iterates over arrays but posts is an object of objects! So we use lodash
+    _.map(this.props.posts, post => {
+      return (
+        <li key={post.id} className="list-group-item">
+          {post.title}
+        </li>
+      );
+    }); 
+  }
 
   render() {
     return(
       <div>
+        <div className="text-xs-right">
+          <Link className="btn btn-primary" to="/posts/new">
+            Add a Post
+          </Link>
+        </div>
         <h3>Posts</h3>
         <ul className="list-group">
           {this.renderPosts()}
