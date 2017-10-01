fcd244a3381e3117de1b3e76c92cb0bc931dd21c
James Attard <info@jamesattard.com>
2017-10-01 12:25:34 +0200

    Added postshow

---
+++ src/components/posts_show.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPost } from '../actions';
+
+class PostsShow extends Component {
+  componentDidMount() {
+    this.props.match.params.id;
+    this.props.fetchPost();
+  }
+
+  render() {
+    return (
+      <div>
+        Posts Show!
+      </div>
+    );
+  };
+}
+
+function mapStateToProps({ posts }) {
+
+}
+
+export default connect(null, { fetchPost })(PostsShow);
